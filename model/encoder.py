6ebba133a906e962f4a0fcbc8e510d6887061d4b
Cole Hendrickson <colehendrickson52@gmail.com>
2024-10-31 16:54:45 -0600

    Finished encoder structure minus positional encoding if necessary

--- model/encoder.py
+++ model/encoder.py
@@ -4,7 +4,7 @@ from torch import nn
 from attention import MultiHeadSelfAttention
 
 class GlobalEncoderLayer(nn.Module):
-    def __init__(self, d_model=512, d_k =64, d_v=64, num_heads=8, drop=0.1):
+    def __init__(self, d_model=512, d_k =64, d_v=64, num_heads=8, drop=0.1, mask=None):
         super(GlobalEncoderLayer, self).__init__()
 
         self.d_model=d_model
@@ -13,7 +13,7 @@ class GlobalEncoderLayer(nn.Module):
         self.num_heads=num_heads
         self.d_ff=d_ff
 
-        self.att_layer = MultiHeadSelfAttention(d_model, d_k, d_v, num_heads)
+        self.att_layer = MultiHeadSelfAttention(d_model, d_k, d_v, num_heads, mask)
         self.ff_layer = nn.Linear(d_model, d_model)
         self.dropout = nn.Dropout(drop)
         self.norm = nn.LayerNorm(d_model)
@@ -23,4 +23,18 @@ class GlobalEncoderLayer(nn.Module):
         x = self.ff_layer(x)
         x = self.norm(F.relu(x))
 
-        return x 
+        return x
+    
+class GlobalEnhancedEncoder(nn.Module):
+    def __init__(self, num_layers, d_model, d_k, d_v, num_heads, drop):
+        super(GlobalEnhancedEncoder, self).__init__()
+        self.num_layers = num_layers
+        self.d_model = d_model
+        self.d_k = d_k
+        self.d_v = d_v
+        self.num_heads = num_heads
+        self.drop = drop
+
+        self.mask = None   # Not sure if necessary or not...
+
+        self.encode_layers = nn.ModuleList([GlobalEncoderLayer(d_model, d_k, d_v, num_heads, drop, self.mask) for i in range(num_layers)])

fa29ec98fab257693ec40bedbde7cc92e97df573
Cole Hendrickson <colehendrickson52@gmail.com>
2024-10-31 14:03:17 -0600

    Getting ready to build data

---
+++ my_train.py
@@ -0,0 +1,64 @@
+import argparse
+import torch
+from common.data.field import ImageDetectionsField, TextField
+from common.data import COCO, DataLoader
+from common.train import train
+from common.utils.utils import create_dataset
+from common.evaluation import PTBTokenizer, Cider
+# from models import build_encoder, build_decoder, Transformer
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Dual Transformer')
+    parser.add_argument('--output', type=str, default='DualModel')
+    parser.add_argument('--exp_name', type=str, default='dft')
+    parser.add_argument('--device', type=str, default='cuda:0')
+    parser.add_argument('--batch_size', type=int, default=20)
+    parser.add_argument('--workers', type=int, default=8)
+
+    parser.add_argument('--N_enc', type=int, default=3)
+    parser.add_argument('--N_dec', type=int, default=3)
+    parser.add_argument('--features_path', type=str)
+    parser.add_argument('--xe_base_lr', type=float, default=1e-4)
+    parser.add_argument('--rl_base_lr', type=float, default=5e-6)
+    parser.add_argument('--use_rl', action='store_true')
+    parser.add_argument('--resume_last', action='store_true')
+    parser.add_argument('--resume_best', action='store_true')
+    parser.add_argument('--clip_path', type=str, default='coco/features/COCO2014_RN50x4_GLOBAL.hdf5')
+    parser.add_argument('--vinvl_path', type=str, default='coco/features/COCO2014_VinVL.hdf5')
+    parser.add_argument('--image_folder', type=str, default='coco/images')
+    parser.add_argument('--annotation_folder', type=str, default='coco/annotations')
+    args = parser.parse_args()
+    print(args)
+
+    return args
+
+
+def main(args):
+    device = torch.device('cuda')
+    args = parse_args()
+    print("Image Captioning Project")
+
+    image_field = ImageDetectionsField(detections_path=args.features_path, max_detections=50, load_in_tmp=False)
+
+    text_field = TextField(init_token='<bos>', eos_token='<eos>', lower=True, tokenize='spacy',
+                           remove_punctuation=True, nopoints=False)
+                           
+
+    # Create the dataset
+    dataset = COCO(image_field, text_field, 'coco/images/', args.annotation_folder, args.annotation_folder)
+    train_dataset, val_dataset, test_dataset = dataset.splits
+
+    if not os.path.isfile('vocab_%s.pkl' % args.exp_name):
+        print("Building vocabulary")
+        text_field.build_vocab(train_dataset, val_dataset, min_freq=5)
+        pickle.dump(text_field.vocab, open('vocab_%s.pkl' % args.exp_name, 'wb'))
+    else:
+        text_field.vocab = pickle.load(open('vocab_%s.pkl' % args.exp_name, 'rb'))
+
+
+    dict_dataset_train = train_dataset.image_dictionary({'image': image_field, 'text': RawField()})
+    ref_caps_train = list(train_dataset.text)
+    cider_train = Cider(PTBTokenizer.tokenize(ref_caps_train))
+    dict_dataset_val = val_dataset.image_dictionary({'image': image_field, 'text': RawField()})
+    dict_dataset_test = test_dataset.image_dictionary({'image': image_field, 'text': RawField()})

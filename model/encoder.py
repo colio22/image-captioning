dfc1c3065af2f9dbc764cc57e2ebf06923d47d03
Cole Hendrickson <colehendrickson52@gmail.com>
2024-11-01 13:03:15 -0600

    Added lstm to encoder

--- model/encoder.py
+++ model/encoder.py
@@ -2,6 +2,7 @@ from torch.nn import functional as F
 import torch
 from torch import nn
 from attention import MultiHeadSelfAttention
+import TensorFlow as tf
 
 class GlobalEncoderLayer(nn.Module):
     def __init__(self, d_model=512, d_k =64, d_v=64, num_heads=8, drop=0.1, mask=None):
@@ -38,3 +39,19 @@ class GlobalEnhancedEncoder(nn.Module):
         self.mask = None   # Not sure if necessary or not...
 
         self.encode_layers = nn.ModuleList([GlobalEncoderLayer(d_model, d_k, d_v, num_heads, drop, self.mask) for i in range(num_layers)])
+        self.lstm_layers = nn.ModuleList([nn.LSTM(input_size=d_model*2, hidden_size=d_model) for i in range(num_layers)])
+        self.final_lstm = nn.LSTM(input_size=d_model*2, hidden_size=d_model)
+
+    def forward(self, x):
+        intermediates = []
+        g = torch.rand(1, self.d_model)
+        for l, e in zip(self.lstm_layers, self.encode_layers):
+            g_in = tf.gather(x, index=[len(x)-1], dim=0)
+            g = torch.cat((g, g_in) -1)
+            g = l.forward(g)
+            x = e.forward(x)
+
+        g = self.final_lstm(g)
+        return x, g
+    
+            

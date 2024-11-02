c0fdc244d0f0a819230f53d05f61bafcc40328e5
Cole Hendrickson <colehendrickson52@gmail.com>
2024-11-02 11:43:58 -0600

    Decoder ready

--- model/attention.py
+++ model/attention.py
@@ -31,23 +31,49 @@ class SelfAttention(nn.Module):
         score = torch.bmm(weights, V)
         return score, weights
 
-    def forward(self, X, mask=None, K=None, V=None):
+    def forward(self, X, mask=None):
         # Connect layers in network
         q = self.q_layer(X)   # Connect input to Queries
-
-        if self.K != None:
-            k = self.k_layer(X)   # Connect input to Keys
-        else:
-            k = self.K
-
-        if self.V != None:
-            v = self.v_layer(X)   # Connect input to Values
-        else:
-            v = self.V
+        k = self.k_layer(X)   # Connect input to Keys
+        v = self.v_layer(X)   # Connect input to Values
 
         # Use existing self-attention function to attend output
         out, weight = self.attention_score(q, k, v, mask)
         return out
+
+
+class CrossAttention(nn.Module):
+    def __init__(self, d_in, d_k, d_v):
+        super(CrossAttention, self).__init__()
+
+        self.d_in = d_in
+        self.d_k = d_k
+        self.d_v = d_v
+
+        # Create fully-connected linear network layers
+        self.q_layer = nn.Linear(d_in, d_k)   # Input to Queries
+
+    def att_score(Q, K, V, mask=None):
+        d_k = Q.size(-1)
+        # Batch multiply Q and K and then scale
+        qk = torch.bmm(Q, torch.transpose(K, 1, 2)) / math.sqrt(d_k)
+
+        if mask != None:
+            qk = qk.masked_fill(mask.logical_not(), -np.inf)
+
+        # Softmax of QK matrix
+        weights = torch.softmax(qk, -1)
+        # Multiply with V
+        score = torch.bmm(weights, V)
+        return score, weights
+
+    def forward(self, X, K, V, mask=None):
+        # Connect layers in network
+        q = self.q_layer(X)   # Connect input to Queries
+
+        # Use existing self-attention function to attend output
+        out, weight = self.attention_score(q, K, V, mask)
+        return out, q
 
     
 class MultiHeadSelfAttention(nn.Module):
@@ -60,17 +86,45 @@ class MultiHeadSelfAttention(nn.Module):
         self.num_heads = num_heads
 
         # Save attention heads as ModuleList for dynamic layer usage
-        self.heads = nn.ModuleList([SelfAttention(d_in, int(d_k/num_heads), int(d_v/num_heads), K=self.K, V=self.V) for i in range(num_heads)])
+        self.heads = nn.ModuleList([SelfAttention(d_in, int(d_k/num_heads), int(d_v/num_heads)) for i in range(num_heads)])
         # Create fully-connected, linear output layer
         self.out = nn.Linear(d_v, d_v)
 
-    def forward(self, X, mask=None, K=None, V=None):
+    def forward(self, X, mask=None):
         # Create empty tensor for concatenated attention head outputs
         outputs = torch.zeros(32, 10, 0)
         # Loop through each attention head layer
         for l in self.heads:
           # Concatenate output of layer with previous outputs
-          outputs = torch.cat((outputs, l.forward(X, mask, K, V)), -1)
+          outputs = torch.cat((outputs, l.forward(X, mask)), -1)
+        # Pass concatenated matrix through fully-connected output layer
+        outputs = self.out(outputs)
+        return outputs
+
+class MultiHeadCrossAttention(nn.Module):
+    def __init__(self, d_in, d_k, d_v, num_heads, mask=None):
+        super(MultiHeadCrossAttention, self).__init__()
+        # Save input parmeters
+        self.d_in = d_in
+        self.d_k = d_k
+        self.d_v = d_v
+        self.num_heads = num_heads
+
+        # Save attention heads as ModuleList for dynamic layer usage
+        self.heads = nn.ModuleList([CrossAttention(d_in, int(d_k/num_heads), int(d_v/num_heads)) for i in range(num_heads)])
+        # Create fully-connected, linear output layer
+        self.out = nn.Linear(d_v, d_v)
+
+    def forward(self, X, K, V, g, mask=None):
+        # Create empty tensor for concatenated attention head outputs
+        outputs = torch.zeros(32, 10, 0)
+        # Loop through each attention head layer
+        for l in self.heads:
+          att, q = l.forward(X, K, V, mask)
+          # Concatenate output of layer with previous outputs
+          outputs = torch.cat((outputs, att), -1)
+
+        outputs = outputs + torch.bmm(q, g)
         # Pass concatenated matrix through fully-connected output layer
         outputs = self.out(outputs)
         return outputs
--- model/decoder.py
+++ model/decoder.py
@@ -1,7 +1,7 @@
 from torch.nn import functional as F
 import torch
 from torch import nn
-from attention import MultiHeadSelfAttention
+from attention import MultiHeadSelfAttention, MultiHeadCrossAttention
 
 class DecoderLayer(nn.Module):
     def __init__(self, d_model, d_k, d_v, num_heads, drop):
@@ -13,8 +13,34 @@ class DecoderLayer(nn.Module):
         self.drop = drop
 
         self.self_att = MultiHeadSelfAttention(d_model, d_k, d_v, num_heads, None)
-        self.cross_att = MultiHeadSelfAttention(d_model, d_k, d_v, num_heads, None)
+        self.cross_att = MultiHeadCrossAttention(d_model, d_k, d_v, num_heads, None)
+
+    def forward(self, x, K, V, g):
+        x = self.self_att(x)
+        x = self.cross_att(x, K, V, g)
+
+        return x
+
+
+class GlobalAdaptiveDecoder(nn.Module):
+    def __init__(self, num_layers, d_model, d_k, d_v, num_heads, drop):
+        super(GlobalAdaptiveDecoder, self).__init__()
+        self.num_layers = num_layers
+        self.d_model = d_model
+        self.d_k = d_k
+        self.d_v = d_v
+        self.num_heads = num_heads
+        self.drop = drop
+
+        self.mask = None  # Need to re-evaluate this...
+
+        self.decode_layers = nn.ModuleList([DecoderLayer(d_model, d_k, d_v, num_heads, drop, self.mask) for i in range(self.num_layers)])
         self.ffn = nn.Linear(d_model, d_model)
 
     def forward(self, x):
-        x = self.self_att(x)
+        for l in self.decode_layers:
+            x = l.forward(x)
+
+        x = torch.softmax(self.ffn(x), -1)
+
+        return x

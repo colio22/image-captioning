9ebd57b2813b699b00bd37e27f0346df3f7ddccc
Cole Hendrickson <colehendrickson52@gmail.com>
2024-11-06 20:40:39 -0700

    Attempting to debug dimension mismatch in encoder

--- model/transformer.py
+++ model/transformer.py
@@ -8,25 +8,28 @@ from model.decoder import GlobalAdaptiveDecoder
 class GlobalEnhancedTransformer(nn.Module):
     def __init__(self, vocab_size, max_len, d_model, d_k, d_v, num_heads, num_layers, drop):
         super(GlobalEnhancedTransformer, self).__init__()
-        self.vocab_size = vocab_size
-        self.max_len = max_len
-        self.d_model = d_model
-        self.d_k = d_k
-        self.d_v = d_v
-        self.num_heads = num_heads
-        self.drop = drop
-        self.num_layers = num_layers
+        self.vocab_size = vocab_size    # Number of words in vocabulary
+        self.max_len = max_len          # Max length of word sequence
+        self.d_model = d_model          # Dimension of model features
+        self.d_k = d_k                  # Demensions of keys and queries
+        self.d_v = d_v                  # Dimensions of attention values
+        self.num_heads = num_heads      # Number of attention heads per layer
+        self.drop = drop                # Percentage to drop in fully connected layers
+        self.num_layers = num_layers    # Number of encoder/decoder layers
 
+        # Encoder Block
         self.encoder = GlobalEnhancedEncoder(num_layers, d_model, d_k, d_v, num_heads, drop)
+        # Decoder Block
         self.decoder = GlobalAdaptiveDecoder(vocab_size, max_len, 0, num_layers, d_model, d_k, d_v, num_heads, drop)
 
     def forward(self, img, seq, mask=None):
-        x = torch.sum(img, dim=0)
+        x = torch.sum(img, -1)
+        print(f'==== Feature dim: {img.shape}. Sum dim: {x.shape}')
         num_padding = len(x) - len(torch.nonzero(x))
-        x = x / (len(x) - num_padding)
-        x = torch.cat([img, x])
-        img_out, g =  self.encoder(x)
-        seq_out = self.decoder(seq, img_out, img_out, g, mask)
+        g = x / (len(x) - num_padding)
+        v = torch.cat([img, g])
+        img_out, g_out =  self.encoder(v)
+        seq_out = self.decoder(seq, img_out, img_out, g_out, mask)
 
         return seq_out
         

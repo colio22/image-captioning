c4edd80e024109e9654a09704e80feabbb77355b
Cole Hendrickson <colehendrickson52@gmail.com>
2024-11-06 22:11:26 -0700

    debug prints

--- model/encoder.py
+++ model/encoder.py
@@ -35,18 +35,21 @@ class GlobalEnhancedEncoder(nn.Module):
         self.num_heads = num_heads      # Number of attention heads
         self.drop = drop                # Percent to drop out
 
-        self.mask = None   # Not sure if necessary or not...
-
+        # Encoder Layers
         self.encode_layers = nn.ModuleList([GlobalEncoderLayer(d_model, d_k, d_v, num_heads, drop, self.mask) for i in range(num_layers)])
+        # LSTM Blocks for global memory
         self.lstm_layers = nn.ModuleList([nn.LSTM(input_size=d_model*2, hidden_size=d_model) for i in range(num_layers)])
+        # Output LSTM block
         self.final_lstm = nn.LSTM(input_size=d_model*2, hidden_size=d_model)
 
     def forward(self, x):
-        intermediates = []
-        g = torch.rand(1, self.d_model)
+        g = torch.rand(1, self.d_model) # Initialize LSTM with random global feature
+        
+        # For each encoder layer and LSTM block
         for l, e in zip(self.lstm_layers, self.encode_layers):
             index = torch.ones([1, self.d_model], device=x.device)
             index = len(x)*index
+            print(f'=== Input tensor: {x.shape}. Index tensor: {index.shape}')
             g_in = torch.gather(x, 0, index.long())  # Use 0 for the dim argument
             g = torch.cat((g, g_in) -1)
             g = l.forward(g)

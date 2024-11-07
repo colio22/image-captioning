802f42bf747c6b598e41698dd1aaa9f23d4fa931
Cole Hendrickson <colehendrickson52@gmail.com>
2024-11-07 15:56:41 -0700

    Putting attention tensors on gpu

--- model/attention.py
+++ model/attention.py
@@ -120,7 +120,7 @@ class MultiHeadSelfAttention(nn.Module):
 
     def forward(self, X, mask=None):
         # Create empty tensor for concatenated attention head outputs
-        outputs = torch.zeros(32, 10, 0)
+        outputs = torch.zeros([50, 51, 0], device=X.device)
         # Loop through each attention head layer
         for l in self.heads:
           # Concatenate output of layer with previous outputs

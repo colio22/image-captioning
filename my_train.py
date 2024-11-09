513213d163d41d69b84b8adcafd0325d8f1c9a0a
Cole Hendrickson <colehendrickson52@gmail.com>
2024-11-09 00:04:39 -0700

    Debug print

--- my_train.py
+++ my_train.py
@@ -44,6 +44,7 @@ def train(model, loss_fn, optimizer, train_loader, tokenizer, epoch=0):
         optimizer.zero_grad()  # Initialize gradients to 0
 
         output = model(img, token_ids, batch_size)    # Put input batch through model
+        print(f"Expected shape: {expected_out.shape}. Model output shape: {output.shape}")
         # loss = loss_fn(output.view(-1, tokenizer.vocab_size), token_ids.view(-1))   # Calculate loss
         loss = loss_fn(output[:,:-1], expected_out.view(50, -1))   # Calculate loss
         loss.backward()        # Update weights
